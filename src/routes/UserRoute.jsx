614e3230afadf42e93c03e833bed62650295119f
Sarfaraz Khan <sarfarazkhan2020@hotmail.com>
2023-04-07 02:15:49 +0530

    comp

---
+++ src/routes/UserRoute.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const UserRoute = () => {
+  return (
+    <div>UserRoute</div>
+  )
+}
---
+++ src/routes/VenderRoute.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const VenderRoute = () => {
+  return (
+    <div>VenderRoute</div>
+  )
+}
