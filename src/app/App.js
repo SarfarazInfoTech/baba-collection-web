3fcdb3efa9c7a9ca3840cfb9bec7d83fb50b83ed
Sarfaraz Khan <sarfarazkhan2020@hotmail.com>
2023-04-07 02:01:49 +0530

    set website

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,11 @@
+import Cart from "../pages/en/Cart";
+import Checkout from "../pages/en/Checkout";
+import Home from "../pages/en/Home";
+import ProductDetails from "../pages/en/ProductDetails";
+import Products from "../pages/en/Products";
+
 function App() {
-  return <div className="App"></div>;
+  return <ProductDetails />;
 }
 
 export default App;
