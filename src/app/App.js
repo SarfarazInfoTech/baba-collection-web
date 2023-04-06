33888b448f47cc0e2b508acfdb296be34f1edc13
Sarfaraz Khan <sarfarazkhan2020@hotmail.com>
2023-04-07 02:05:12 +0530

    done

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,12 @@
-import Cart from "../pages/en/Cart";
-import Checkout from "../pages/en/Checkout";
 import Home from "../pages/en/Home";
-import ProductDetails from "../pages/en/ProductDetails";
 import Products from "../pages/en/Products";
+import ProductDetails from "../pages/en/ProductDetails";
+import Checkout from "../pages/en/Checkout";
+import Cart from "../pages/en/Cart";
+import Contact from "../pages/en/Contact";
 
 function App() {
-  return <ProductDetails />;
+  return <Home />;
 }
 
 export default App;
