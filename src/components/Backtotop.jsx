7c9b5433be65d2c4c8aa20ccb843f4456a463bf8
Sarfaraz Khan <sarfarazkhan2020@hotmail.com>
2023-04-07 15:27:08 +0530

    comp

---
+++ src/components/Backtotop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Backtotop = () => {
+  return (
+    <a href="#" className="btn btn-primary back-to-top">
+      <i className="fa fa-angle-double-up" />
+    </a>
+  );
+};
+
+export default Backtotop;
