e6f814e86c24a91de04e3b313bd2b3492481fe79
Sarfaraz Khan <sarfarazkhan2020@hotmail.com>
2023-05-28 21:05:15 +0530

    Routes done

--- src/app/App.js
+++ src/app/App.js
@@ -1,54 +1,33 @@
+import { authProtectedRoutes, publicRoutes } from "../routes/UserRoute";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/en/Home";
-import Products from "../pages/en/Products";
-import ProductDetails from "../pages/en/ProductDetails";
-import Checkout from "../pages/en/Checkout";
-import Contact from "../pages/en/Contact";
-import Cart from "../pages/en/Cart";
-import About from "../pages/en/About";
-import AdminLogin from "../pages/admin/AdminLogin";
-import AdminSignup from "../pages/admin/AdminSignup";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import Thankyou from "../pages/en/Thankyou";
-import Login from "../pages/users/Login";
-import Signup from "../pages/users/Signup";
-import ShopDetails from "../pages/vender/ShopDetails";
-import VendorRegistration from "../pages/vender/VendorRegistration";
-import UpdateProfile from "../pages/profile/UpdateProfile";
-import OrderHistory from "../pages/orders/OrderHistory";
+import Authmiddleware from "../routes/Authmiddleware";
+import NonAuthLayout from "../routes/NonAuthLayout";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* End-User */}
-        <Route path="/" element={<Home />} />
-        <Route path="/ShopDetails" element={<ShopDetails />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/Products" element={<Products />} />
-        <Route path="Products/:keyword" element={<Products />} />
-        <Route path="/ProductDetails" element={<ProductDetails />} />
-        <Route path="/Product/:id" element={<ProductDetails />} />
-        {/* <Route path="/Product/:id" element={<ProductDetails />} /> */}
-        <Route path="/Checkout" element={<Checkout />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/thankyou" element={<Thankyou />} />
-        <Route path="/edit-profile" element={<UpdateProfile />} />
-        <Route path="/order-history" element={<OrderHistory />} />
-        <Route path="/help-center" element={<OrderHistory />} />
+    <>
+      <BrowserRouter>
+        <Routes>
+          {publicRoutes.map((route, idx) => (
+            <Route
+              path={route.path}
+              element={<NonAuthLayout>{route.component}</NonAuthLayout>}
+              key={idx}
+              exact={true}
+            />
+          ))}
 
-        {/* Admin-user */}
-        <Route path="/Admin-Login" element={<AdminLogin />} />
-        <Route path="/Admin-Signup" element={<AdminSignup />} />
-        <Route path="/Admin-Dashboard" element={<AdminDashboard />} />
-
-        {/* Vendor */}
-        <Route path="/VendorRegistration" element={<VendorRegistration />} />
-      </Routes>
-    </BrowserRouter>
+          {authProtectedRoutes.map((route, idx) => (
+            <Route
+              path={route.path}
+              element={<Authmiddleware>{route.component}</Authmiddleware>}
+              key={idx}
+              exact={true}
+            />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/routes/UserRoute.jsx
+++ src/routes/UserRoute.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from "react";
+import About from "../pages/en/About";
+import Cart from "../pages/en/Cart";
+import Checkout from "../pages/en/Checkout";
+import Contact from "../pages/en/Contact";
+import Home from "../pages/en/Home";
+import ProductDetails from "../pages/en/ProductDetails";
+import Products from "../pages/en/Products";
+import Thankyou from "../pages/en/Thankyou";
+import OrderHistory from "../pages/orders/OrderHistory";
+import UpdateProfile from "../pages/profile/UpdateProfile";
+import Login from "../pages/users/Login";
+import Signup from "../pages/users/Signup";
+import ShopDetails from "../pages/vender/ShopDetails";
+import VendorRegistration from "../pages/vender/VendorRegistration";
 
-export const UserRoute = () => {
-  return (
-    <div>UserRoute</div>
-  )
+{
+  /* <Routes>
+  <Route path="/Admin-Login" element={<AdminLogin />} />
+  <Route path="/Admin-Signup" element={<AdminSignup />} />
+  <Route path="/Admin-Dashboard" element={<AdminDashboard />} />
+</Routes> */
 }
+
+const authProtectedRoutes = [
+  { path: "/edit-profile", component: <UpdateProfile /> },
+  { path: "/order-history", component: <OrderHistory /> },
+  { path: "/help-center", component: <OrderHistory /> },
+];
+
+const publicRoutes = [
+  { path: "/", component: <Home /> },
+  { path: "/login", component: <Login /> },
+  { path: "/signup", component: <Signup /> },
+  { path: "/products", component: <Products /> },
+  { path: "/Products/:keyword", component: <Products /> },
+  { path: "/Product/:id", component: <Products /> },
+  { path: "/ProductDetails", component: <ProductDetails /> },
+  { path: "/ShopDetails", component: <ShopDetails /> },
+  { path: "/Checkout", component: <Checkout /> },
+  { path: "/Cart", component: <Cart /> },
+  { path: "/Contact", component: <Contact /> },
+  { path: "/About", component: <About /> },
+  { path: "/thankyou", component: <Thankyou /> },
+  { path: "/VendorRegistration", component: <VendorRegistration /> },
+];
+
+export { authProtectedRoutes, publicRoutes };
